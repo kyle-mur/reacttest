aa592d948350d6b195cac81e180d5dddb2db617a
Kyle <murphykyuk@gmail.com>
2020-09-08 00:33:52 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router, Route, Switch, Redirect, Link,useHistory, useParams} from 'react-router-dom';
+import { withRouter } from 'react-router'
 const api = 'https://cors-anywhere.herokuapp.com/api.brewerydb.com/v2/beer/';
 const breweryApi = 'https://cors-anywhere.herokuapp.com/api.brewerydb.com/v2/brewery/';
 const randomBeer = 'https://cors-anywhere.herokuapp.com/api.brewerydb.com/v2/beer/random/';
 
 const apiKey = "?withBreweries=Y&hasLabels=Y&key=a5c1b917e7ba62dcd79f434ed73bc72d";
+
+// has labels does not work with random for some reason
 
 class Brewery extends React.Component {
 
@@ -135,6 +138,8 @@ class Beer extends React.Component {
         name: this.state.data.name,
         description: this.state.data.style.description,
         labels: this.state.data.labels || {} // not every beer has a label
+        // actually it seems that a random beer request doesn't return a label even if we ask for one, yet a direct request does
+        // so this is broken on the API perhaps
       }
       brewery = this.state.data.breweries? this.state.data.breweries[0] : {};
     }
@@ -187,12 +192,20 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.goBack = this.goBack.bind(this);
   }
+
+  goBack(){
+    console.log(this)
+    return this.props.history ? this.props.history.goBack() : null;
+  }
+
 
   render() {
 
     return (
-      <div>
+      <div className="container"><Header></Header>
+
         <Switch>
         {routes.map((route, i) => (
           <RouteWithSubRoutes key={i} {...route} />
@@ -207,6 +220,23 @@ class App extends React.Component {
   }
 }
 
+
+class HeaderComponent extends React.Component {
+  render() {
+    return (
+
+      <div className="page-header">
+      <div><a href="#" onClick={() => this.props.history.go(-1)} >Go Back</a></div>
+      <div className="pull-left"><h3>The Random Beer App</h3></div>
+
+      <div className="pull-right"><a href="/" className="btn"><h3>Get Another Beer</h3></a></div>
+<div class="clearfix"></div>      </div>
+    )
+  }
+}
+
+
+const Header = withRouter(HeaderComponent);
 
 
 const fetchApi = async (request) => {
@@ -226,13 +256,9 @@ const fetchApi = async (request) => {
 }
 
 
-
 ReactDOM.render(
-  <div className="container">
-  <div className="page-header"><h3>The Random Beer App</h3></div>
   <Router>
   <App />
-  </Router>
-  </div>,
+  </Router>,
   document.getElementById('root')
 );
